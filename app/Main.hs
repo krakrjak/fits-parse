34efade809af09584f21a0c6dcdea61dd4818eeb
Zac Slade <krakrjak@gmail.com>
2018-06-28 01:44:15 -0500

    Add option parsing to the render app

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,60 @@
 module Main where
 
+import Data.Monoid( (<>) )
+import Control.Applicative( (<|>), (<**>) )
+import Options.Applicative( Parser, strOption, optional, short, long
+                          , flag', metavar, help, execParser, info
+                          , fullDesc, header, helper, progDesc
+                          , argument, str )
+
+data Input = FileInput FilePath | StdInput
+    deriving (Show)
+data Output = FileOutput FilePath | StdOutput
+    deriving (Show)
+
+fileInput :: Parser Input
+fileInput = FileInput <$> argument str
+    ( metavar "FILE"
+   <> help "Input FITS file to parse" )
+
+stdInput :: Parser Input
+stdInput = flag' StdInput
+    ( long "stdin"
+   <> help "Input FITS from stdin" )
+
+fileOutput :: Parser Output
+fileOutput = FileOutput <$> strOption
+    ( long "outfile"
+   <> short 'o'
+   <> help "Output filename to store" )
+
+stdOutput :: Parser Output
+stdOutput = flag' StdOutput
+    ( long "stdout"
+   <> help "Output picture to stdout" )
+
+inputParse :: Parser Input
+inputParse = fileInput <|> stdInput
+
+outputParse :: Parser Output
+outputParse = fileOutput <|> stdOutput
+
+data FitsConfig = FitsConfig
+    { infile :: Input
+    , outImage :: Maybe Output
+    }
+    deriving (Show)
+
+fitsConfigParser :: Parser FitsConfig
+fitsConfigParser = FitsConfig <$> inputParse <*> optional outputParse
+
 main :: IO ()
-main = do
-  putStrLn "hello world"
+main = workOnFITS =<< execParser opts
+    where
+        opts = info (fitsConfigParser <**> helper)
+            ( fullDesc
+           <> progDesc "Parse, generate, and render FITS files"
+           <> header "fits-parse - a FITS swiss army knife" )
+
+workOnFITS :: FitsConfig -> IO ()
+workOnFITS f = return ()
