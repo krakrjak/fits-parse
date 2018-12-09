e6097b4709318cfb235a4ed26ee0fe197de8e501
Zac Slade <krakrjak@gmail.com>
2018-12-09 04:31:22 -0600

    [refactor, feature] upgrade to megaparsec-7, add permutation parsing

--- src/Data/Fits/MegaParser.hs
+++ src/Data/Fits/MegaParser.hs
@@ -19,6 +19,8 @@ import qualified Data.ByteString.Internal as BS ( c2w )
 import qualified Text.Megaparsec as M
 import qualified Text.Megaparsec.Byte as M
 import qualified Text.Megaparsec.Byte.Lexer as MBL
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 
 -- explicit imports
 import Numeric.Natural ( Natural )
@@ -27,20 +29,18 @@ import Data.Word ( Word8, Word16, Word32, Word64 )
 import Data.Void ( Void )
 import Data.Default ( def )
 import Text.Ascii ( isAscii )
+import Text.Megaparsec ( Parsec, ParseErrorBundle, (<?>))
 
 -- full module imports
-import Text.Megaparsec ( Parsec, ParseError, (<|>))
+---- parser-combinators
+import Control.Applicative.Combinators
+import Control.Applicative.Permutations
 
 -- local imports
 import Data.Fits
 
 type Parser = Parsec Void ByteString
-type ParseErr = ParseError Word8 Void
-
-data HeaderValues
-  = FITSStrStrKwd StringValue StringValue
-  | FITSStrNumKwd StringValue NumberValue
-  | END
+type ParseErr = ParseErrorBundle Word8 Void
 
 data DataUnitValues
   = FITSUInt8 Word8
@@ -48,20 +48,25 @@ data DataUnitValues
   | FITSInt32 Word32
   | FITSInt64 Word64
   | FITSFloat32 Float
-  | FITSFloat64 Double
+  | FITSFloat64 Double 
 
 headerBlockParse :: Parser HeaderData
 headerBlockParse = do
-    simple <- parseSimple
-    bitpix <- parseBitPix
-    axisnum <- parseAxisCount
-    axesDesc <- parseNaxes axisnum
     -- TODO: Parse other sections of the neader for god's sake
-    parseEnd
-    return defHeader { simpleFormat = simple
+    (simple, bitpix, axesDesc, bZero, bScale, pCreator, pDate) <- runPermutation $ 
+        (,,,,,,) <$> toPermutation (parseSimple <?> "simple")
+               <*> toPermutation (parseBitPix <?> "bitpix")
+               <*> toPermutation ((parseAxisCount >>= parseNaxes) <?> "axis parsing")
+               <*> toPermutationWithDefault 0 (parseBzero <?> "bzero")
+               <*> toPermutationWithDefault 0 (parseBscale <?> "bscale")
+               <*> toPermutationWithDefault (StringValue NullString Nothing) (parseCreator <?> "creator")
+               <*> toPermutationWithDefault (StringValue NullString Nothing) (parseDate <?> "date")
+    parseEnd >> return defHeader { simpleFormat = simple
                      , bitPixFormat = bitpix
-                     , nAxisMetaData = parsedNAxisMeta axisnum
-                     , axes = axesDesc }
+                     , nAxisMetaData = parsedNAxisMeta (length axesDesc)
+                     , axes = axesDesc
+                     , observationDate = pDate
+                     , authorIdentifier = pCreator }
   where
     defHeader = def :: HeaderData
     defNAxisMeta = def :: NAxisMetadata
@@ -75,20 +80,20 @@ parseSimple = M.string' "simple" >> parseEquals
     >> ((conformParse >> consumeDead >> return Conformant)
     <|> (nonConformParse >> consumeDead >> return NonConformant))
   where
-    conformParse = M.string' "t"
-    nonConformParse = M.anyChar
+    conformParse = M.single (BS.c2w 't')
+    nonConformParse = M.anySingle
 
 parseEquals :: Parser ()
-parseEquals = M.space >> M.char (BS.c2w '=') >> M.space
+parseEquals = M.space >> M.single (BS.c2w '=') >> M.space
 
 parseBitPix :: Parser BitPixFormat
-parseBitPix = M.string' "bitpix" >> parseEquals
-    >> ((M.string' "8" >> consumeDead >> return EightBitInt)
-    <|> (M.string' "16" >> consumeDead >> return SixteenBitInt)
-    <|> (M.string' "32" >> consumeDead >> return ThirtyTwoBitInt)
-    <|> (M.string' "64" >> consumeDead >> return SixtyFourBitInt)
-    <|> (M.string' "-32" >> consumeDead >> return ThirtyTwoBitFloat)
-    <|> (M.string' "-64" >> consumeDead >> return SixtyFourBitFloat))
+parseBitPix  = M.string' "bitpix" >> parseEquals
+    >> ((M.chunk "8" >> consumeDead >> return EightBitInt)
+    <|> (M.chunk "16" >> consumeDead >> return SixteenBitInt)
+    <|> (M.chunk "32" >> consumeDead >> return ThirtyTwoBitInt)
+    <|> (M.chunk "64" >> consumeDead >> return SixtyFourBitInt)
+    <|> (M.chunk "-32" >> consumeDead >> return ThirtyTwoBitFloat)
+    <|> (M.chunk "-64" >> consumeDead >> return SixtyFourBitFloat))
 
 parseAxisCount :: Parser Natural
 parseAxisCount = M.string' "naxis" >> parseEquals >> parseNatural
@@ -104,6 +109,18 @@ parseNaxes n          = do
       buildAxis an ec = defAxis { axisNumber = fromIntegral an
                                 , axisElementCount = fromIntegral ec }
 
+parseBzero :: Parser Int
+parseBzero = M.string' "bzero" >> parseEquals >> parseInteger
+
+parseBscale :: Parser Int
+parseBscale = M.string' "bscale" >> parseEquals >> parseInteger
+
+parseCreator :: Parser StringValue
+parseCreator = M.string' "creator" >> parseEquals >> parseStringValue
+
+parseDate :: Parser StringValue
+parseDate = M.string' "date" >> parseEquals >> parseStringValue
+
 skipEmpty :: Parser ()
 skipEmpty = (M.many $ M.satisfy ((0::Word8) ==)) >> return ()
 
@@ -111,13 +128,34 @@ consumeDead :: Parser ()
 consumeDead = M.space >> skipEmpty
 
 parseEnd :: Parser ()
-parseEnd = M.string' "end" >> consumeDead
+parseEnd = M.string' "end" >> consumeDead >> M.takeRest >> return ()
 
 parseNatural :: Parser Natural
 parseNatural = do
     v <- MBL.decimal
     consumeDead
     return $ fromIntegral v
+
+parseInteger :: Parser Int
+parseInteger = do
+    v <- MBL.decimal
+    consumeDead
+    return v
+
+parseStringValue :: Parser StringValue
+parseStringValue = do 
+       -- The rules are weird, NULL means a NULL string, '' is an empty
+       -- string, a ' followed by a bunch of spaces and a close ' is
+       -- considered an empty string, and trailing whitespace is ignored
+       -- within the quotes, but not leading spaces. 
+    v <- (M.try parseInQuotes) <|> return []
+    let bs = BS.pack v
+    if BS.length bs < 1
+        then consumeDead >> return (StringValue NullString Nothing)
+        else consumeDead >> return (StringValue DataString (Just $ TE.decodeUtf8 bs))
+
+parseInQuotes :: Parser [Word8]
+parseInQuotes = between (M.single 39) (M.single 39) $ M.many M.anySingle
 
 countHeaderDataUnits :: ByteString -> IO Natural
 countHeaderDataUnits bs = getAllHDUs bs >>= return . fromIntegral . length
@@ -138,7 +176,7 @@ getOneHDU bs =
           Right mainHeader -> do
             let (dataUnit, remainder) = BS.splitAt (fromIntegral $ dataSize mainHeader) rest
             return (HeaderDataUnit mainHeader dataUnit, remainder)
-          Left e -> let err = M.parseErrorPretty e in error err
+          Left e -> let err = M.errorBundlePretty e in print header >> error err
   where
     (header, rest) = BS.splitAt hduBlockSize bs
 
