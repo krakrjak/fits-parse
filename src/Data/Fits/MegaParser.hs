8eeedb878a63ef7acbbf2032d3d5d7352240b87f
Zac Slade <krakrjak@gmail.com>
2018-12-18 15:23:45 -0600

    [refactor, feature] using permutation parser works really well

--- src/Data/Fits/MegaParser.hs
+++ src/Data/Fits/MegaParser.hs
@@ -17,30 +17,34 @@ module Data.Fits.MegaParser where
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Internal as BS ( c2w )
 import qualified Text.Megaparsec as M
-import qualified Text.Megaparsec.Byte as M
-import qualified Text.Megaparsec.Byte.Lexer as MBL
+import qualified Text.Megaparsec.Stream as M
+import qualified Text.Megaparsec.Char as M
+import qualified Text.Megaparsec.Char.Lexer as MCL
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 
 -- explicit imports
+import Control.Monad ( void )
 import Numeric.Natural ( Natural )
 import Data.ByteString ( ByteString )
+import Data.Text ( Text )
 import Data.Word ( Word8, Word16, Word32, Word64 )
 import Data.Void ( Void )
 import Data.Default ( def )
 import Text.Ascii ( isAscii )
-import Text.Megaparsec ( Parsec, ParseErrorBundle, (<?>))
+import Text.Megaparsec ( Parsec, ParseErrorBundle, (<|>), (<?>))
 
 -- full module imports
 ---- parser-combinators
-import Control.Applicative.Combinators
 import Control.Applicative.Permutations
+---- base
+import Data.Proxy
 
 -- local imports
 import Data.Fits
 
-type Parser = Parsec Void ByteString
-type ParseErr = ParseErrorBundle Word8 Void
+type Parser = Parsec Void Text
+type ParseErr = ParseErrorBundle Text Void
 
 data DataUnitValues
   = FITSUInt8 Word8
@@ -53,15 +57,16 @@ data DataUnitValues
 headerBlockParse :: Parser HeaderData
 headerBlockParse = do
     -- TODO: Parse other sections of the neader for god's sake
-    (simple, bitpix, axesDesc, bZero, bScale, pCreator, pDate) <- runPermutation $ 
-        (,,,,,,) <$> toPermutation (parseSimple <?> "simple")
+    (simple, bitpix, axesDesc, bZero, bScale, pCreator, pDate, pEnd) <- runPermutation $
+        (,,,,,,,) <$> toPermutation (parseSimple <?> "simple")
                <*> toPermutation (parseBitPix <?> "bitpix")
                <*> toPermutation ((parseAxisCount >>= parseNaxes) <?> "axis parsing")
                <*> toPermutationWithDefault 0 (parseBzero <?> "bzero")
                <*> toPermutationWithDefault 0 (parseBscale <?> "bscale")
                <*> toPermutationWithDefault (StringValue NullString Nothing) (parseCreator <?> "creator")
                <*> toPermutationWithDefault (StringValue NullString Nothing) (parseDate <?> "date")
-    parseEnd >> return defHeader { simpleFormat = simple
+               <*> toPermutation (parseEnd <?> "end")
+    return defHeader { simpleFormat = simple
                      , bitPixFormat = bitpix
                      , nAxisMetaData = parsedNAxisMeta (length axesDesc)
                      , axes = axesDesc
@@ -80,11 +85,11 @@ parseSimple = M.string' "simple" >> parseEquals
     >> ((conformParse >> consumeDead >> return Conformant)
     <|> (nonConformParse >> consumeDead >> return NonConformant))
   where
-    conformParse = M.single (BS.c2w 't')
+    conformParse = M.char' 't'
     nonConformParse = M.anySingle
 
 parseEquals :: Parser ()
-parseEquals = M.space >> M.single (BS.c2w '=') >> M.space
+parseEquals = M.space >> M.char '=' >> M.space
 
 parseBitPix :: Parser BitPixFormat
 parseBitPix  = M.string' "bitpix" >> parseEquals
@@ -122,61 +127,62 @@ parseDate :: Parser StringValue
 parseDate = M.string' "date" >> parseEquals >> parseStringValue
 
 skipEmpty :: Parser ()
-skipEmpty = (M.many $ M.satisfy ((0::Word8) ==)) >> return ()
+skipEmpty = (M.many $ M.satisfy (('\0') ==)) >> return ()
 
 consumeDead :: Parser ()
 consumeDead = M.space >> skipEmpty
 
 parseEnd :: Parser ()
-parseEnd = M.string' "end" >> consumeDead >> M.takeRest >> return ()
+parseEnd = M.string' "end" >> M.space <* M.eof
 
 parseNatural :: Parser Natural
 parseNatural = do
-    v <- MBL.decimal
+    v <- MCL.decimal
     consumeDead
     return $ fromIntegral v
 
 parseInteger :: Parser Int
 parseInteger = do
-    v <- MBL.decimal
+    v <- MCL.decimal
     consumeDead
     return v
 
 parseStringValue :: Parser StringValue
 parseStringValue = do 
-       -- The rules are weird, NULL means a NULL string, '' is an empty
-       -- string, a ' followed by a bunch of spaces and a close ' is
-       -- considered an empty string, and trailing whitespace is ignored
-       -- within the quotes, but not leading spaces. 
-    v <- (M.try parseInQuotes) <|> return []
-    let bs = BS.pack v
-    if BS.length bs < 1
-        then consumeDead >> return (StringValue NullString Nothing)
-        else consumeDead >> return (StringValue DataString (Just $ TE.decodeUtf8 bs))
-
-parseInQuotes :: Parser [Word8]
-parseInQuotes = between (M.single 39) (M.single 39) $ M.many M.anySingle
+    -- The rules are weird, NULL means a NULL string, '' is an empty
+    -- string, a ' followed by a bunch of spaces and a close ' is
+    -- considered an empty string, and trailing whitespace is ignored
+    -- within the quotes, but not leading spaces.
+    ls <- M.between (M.char '\'') (M.char '\'') $ M.many $ M.anySingleBut '\''
+    consumeDead
+    let v = M.tokensToChunk (Proxy :: Proxy Text) ls
+    if T.length v < 1
+      then return (StringValue EmptyString Nothing)
+      else return (StringValue DataString (Just v))
 
 countHeaderDataUnits :: ByteString -> IO Natural
 countHeaderDataUnits bs = getAllHDUs bs >>= return . fromIntegral . length
 
+-- TODO: make the recursive case work. Currently limited to one HDU.
+-- The current issue is that when the parser fails on an HDU parse, it
+-- blows them all up instead of accepting the valid parsings.
 getAllHDUs :: ByteString -> IO [HeaderDataUnit]
 getAllHDUs bs = do
     (hdu, rest) <- getOneHDU bs
     case BS.length rest < hduBlockSize of
       True  -> return [hdu]
-      False -> fmap ([hdu] ++) $ getAllHDUs rest
+      False -> return [hdu] -- fmap ([hdu] ++) $ getAllHDUs rest
 
 getOneHDU :: ByteString -> IO (HeaderDataUnit, ByteString)
 getOneHDU bs =
     case isAscii header of
       False -> error "Header data is not ASCII. Please Check your input file and try again"
       True  -> do
-        case M.runParser headerBlockParse "FITS" header of
+        case M.runParser headerBlockParse "FITS" (TE.decodeUtf8 header) of
           Right mainHeader -> do
             let (dataUnit, remainder) = BS.splitAt (fromIntegral $ dataSize mainHeader) rest
             return (HeaderDataUnit mainHeader dataUnit, remainder)
-          Left e -> let err = M.errorBundlePretty e in print header >> error err
+          Left e -> let err = M.errorBundlePretty e in error err
   where
     (header, rest) = BS.splitAt hduBlockSize bs
 
